d16177948682602b2e98f71055f31aae05cc7021
13516127 Muhammad Azka Widyanto <13516127@std.stei.itb.ac.id>
2019-11-05 18:32:59 +0700

    adding ADT Command

---
+++ command.h
@@ -0,0 +1,20 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+
+#include "bangunan.h"
+#include "boolean.h"
+
+
+
+void attack();
+void level_up();
+void skill();
+void undo();
+void end_turn();
+//void save();
+//void move();
+void exit();
+
+
+#endif
