837fd010b1d695a51be7ccd8e64a58181c7f7ee6
Rizki <mrizkinash@gmail.com>
2019-11-11 22:44:47 +0700

    added more on main

--- main.c
+++ main.c
@@ -28,9 +28,55 @@ int KataToInt (Kata CKata){
 
     return retval;
 }
+
+void initBangunan (Bangunan *B, char CC, int X, int Y){
+
+    setBangunanType(B, CC);
+    setBangunanLevel(B, 1);
+    setBangunanPoint(B, X, Y);
+
+    if (CC == 'C'){
+
+        (*B).gain = 10;
+        (*B).max = 40;
+        initArmy(B, 40);
+        setBangunanShield(B, false);
+    }
+    else if (CC == 'T'){
+
+        (*B).gain = 5;
+        (*B).max = 20;
+        initArmy(B, 30);
+        setBangunanShield(B, true);
+    }
+    else if (CC == 'F'){
+
+        (*B).gain = 10;
+        (*B).max = 20;
+        initArmy(B, 80);
+        setBangunanShield(B, false);
+    }
+    else if (CC == 'V'){
+
+        (*B).gain = 5;
+        (*B).max = 20;
+        initArmy(B, 20);
+        setBangunanShield(B, false);
+    }
+    else{
+
+        (*B).gain = 0;
+        (*B).max = 0;
+        initArmy(B, 0);
+        setBangunanShield(B, false);
+    }
+}
+
 int main(){
     int counter, tinggi, lebar, i, j;
     MATRIKS M;
+    Bangunan B;
+    char type;
 
     counter = 1;
     STARTKATA()         // Baca dari file config
@@ -49,7 +95,8 @@ int main(){
 
                     if ((i == 1) || (i == M.NBrsEff) || (j == 1) || (j == M.NKolEff)){
 
-                        Elmt(M, i, j) = '*'
+                        initBangunan(&B, '*', i, j);
+                        Elmt(M, i, j) = B;
                     }
                 }
             }
@@ -58,10 +105,27 @@ int main(){
         else if(counter == 2){
 
             counter += KataToInt(CKata);
+            ADVKATA();
             while (counter > 2){
 
-            // Bikin bangunan disini
+                // Bikin bangunan disini
+                type = CKata.TabKata[1];
+                ADVKATA();
+                i = KataToInt(CKata);
+                ADVKATA();
+                j = KataToInt(CKata);
+
+                initBangunan(&B, type, i, j);
+                Elmt(M, i, j) = B;  // Bangunan dijadikan elemen matriks of bangunan
+
+                ADVKATA();
+                counter--
             }
+            counter++;
+        }
+        else if (counter == 3){
+
+            // Keterhubungan bangunan disini
         }
     }
 
