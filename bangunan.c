3c9fe87ce8373bdbc0d05da8c68f40cca2e706cb
cacachandrika <chandrika1302@gmail.com>
2019-11-17 16:59:17 +0700

    Update bangunan.c

--- bangunan.c
+++ bangunan.c
@@ -1,50 +1,46 @@
+#include <stdio.h>
 #include "bangunan.h"
-#include "ADT/boolean.h"
-#include "ADT/point.h"
 
-int getArmy(Bangunan B)
-{
-	return(Army(B));
+void LevelUpBangunan(Bangunan B){
+    if(Type(B)=='C') Army(B)-MC[Level(B)];
+	else if(Type(B)=='T') Army(B)-MT[Level(B)];
+	else if(Type(B)=='F') Army(B)-MF[Level(B)];
+	else if(Type(B)=='V') Army(B)-MV[Level(B)];
+    Level(B)+=1;
 }
+// level +=1
+// jumlah pasukan berkurang M/2
 
-boolean getBangunanShield(Bangunan B)
-{
-	return(Shield(B));
+Boolean CekBatasPasukan(Bangunan B){
+    if(Type(B)=='C') return (Army(B)+AC[Level(B)] < MC[Level(B)]);
+	else if(Type(B)=='T') return (Army(B)+AT[Level(B)] < MT[Level(B)]);
+	else if(Type(B)=='F') return (Army(B)+AF[Level(B)] < MF[Level(B)]);
+	else if(Type(B)=='V') return (Army(B)+AV[Level(B)] < MV[Level(B)]);
 }
-int getBangunanLevel(Bangunan B)
-{
-	return (Level(B));
+// Cek apakah nilai dari penambahan pasukan melebihi M
+// jika sudah melebihi, stop penambahan
+
+void AddPasukan(Bangunan B){
+	if(CekBatasPasukan(B) && Owner(B)!=0){
+		if(Type(B)=='C') Army(B)+=AC[Level(B)];
+		else if(Type(B)=='T') Army(B)+=AT[Level(B)];
+		else if(Type(B)=='F') Army(B)+=AF[Level(B)];
+		else if(Type(B)=='V') Army(B)+=AV[Level(B)];
+	}
 }
-int getBangunanIndex(Bangunan B)
-{
-	return (Index(B));
+// menambahkan A jumlah pasukan ke seluruh bangunan yang dimiliki X pada setiap turn X
+
+void ChangeOwnerB(Bangunan B, int X, int Y){
+    Level(B) = 1;
+    Army(B) = X;
+    Owner(B) = Y;
 }
-char getBangunanType(Bangunan B)
-{
-	return (Type(B));
+// reset bangunan saat pindah kepemilikan, 
+// dengan X sebagai jumlah pasukan yang baru
+// dan Y sebagai pemilik yang baru
+
+void MovePasukan(BangArr Br, int X, int Y, int pas){
+    Army(Br.arr[X])-=pas;
+    Army(Br.arr[Y])+=pas;
 }
-void setBangunanPoint(Bangunan *B, int X, int Y)
-{
-	AbsisBangunan(*B) = X;
-	OrdinatBangunan(*B) = Y;
-}
-void setBangunanType(Bangunan *B, char CC)
-{
-	Type(*B) = CC;
-}
-void setBangunanIndex(Bangunan *B,int i)
-{
-	Index(*B) = i;
-}
-void initArmy(Bangunan *B, int U)
-{
-	Army(*B) = U;
-}
-void setBangunanShield(Bangunan *B, boolean X)
-{
-	Shield(*B) = X;
-}
-void setBangunanLevel(Bangunan *B, int X)
-{
-	Level(*B) = X;
-}	
+// memindahkan pas buah pasukan dari bangunan X ke bangunan Y
