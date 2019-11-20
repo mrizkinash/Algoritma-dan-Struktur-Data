cec756309fde5a2fa0d858c537d80b51134202cb
cacachandrika <chandrika1302@gmail.com>
2019-11-20 11:53:41 +0700

    Update bangunan.c

--- bangunan.c
+++ bangunan.c
@@ -2,27 +2,27 @@
 #include "bangunan.h"
 
 void CreateBangunan (Bangunan *B, char c, int i, int j){
-	Type(B) = c;
-    AbsisBangunan(B) = i;
-    OrdinatBangunan(B) = j;
-	Level(B)=1;
-	if(Type(B)=='C') Army(B)=40;
-	else if(Type(B)=='T') Army(B)=30;
-	else if(Type(B)=='F') Army(B)=80;
-	else if(Type(B)=='V') Army(B)=20;
+	Type(*B) = c;
+    AbsisBangunan(*B) = i;
+    OrdinatBangunan(*B) = j;
+	Level(*B)=1;
+	if(Type(*B)=='C') Army(*B)=40;
+	else if(Type(*B)=='T') Army(*B)=30;
+	else if(Type(*B)=='F') Army(*B)=80;
+	else if(Type(*B)=='V') Army(*B)=20;
 }
 
 void LevelUpBangunan(Bangunan *B){
-    if(Type(B)=='C') Army(B)-MC[Level(B)];
-	else if(Type(B)=='T') Army(B)-MT[Level(B)];
-	else if(Type(B)=='F') Army(B)-MF[Level(B)];
-	else if(Type(B)=='V') Army(B)-MV[Level(B)];
+    if(Type(*B)=='C') Army(B)-(0.5 * MC[Level(*B)]);
+	else if(Type(*B)=='T') Army(B)-(0.5 * MT[Level(*B)]);
+	else if(Type(*B)=='F') Army(B)-(0.5 * MF[Level(*B)]);
+	else if(Type(*B)=='V') Army(B)-(0.5 * MV[Level(*B)]);
     Level(B)+=1;
 }
 // level +=1
 // jumlah pasukan berkurang M/2
 
-Boolean CekBatasPasukan(Bangunan *B){
+Boolean CekBatasPasukan(Bangunan B){
     if(Type(B)=='C') return (Army(B)+AC[Level(B)] >= MC[Level(B)]);
 	else if(Type(B)=='T') return (Army(B)+AT[Level(B)] >= MT[Level(B)]);
 	else if(Type(B)=='F') return (Army(B)+AF[Level(B)] >= MF[Level(B)]);
@@ -34,26 +34,26 @@ Boolean CekBatasPasukan(Bangunan *B){
 
 
 void AddPasukan(Bangunan *B){
-	if(CekBatasPasukan(B) && Owner(B)!=0){
-		if(Type(B)=='C') Army(B)+=AC[Level(B)];
-		else if(Type(B)=='T') Army(B)+=AT[Level(B)];
-		else if(Type(B)=='F') Army(B)+=AF[Level(B)];
-		else if(Type(B)=='V') Army(B)+=AV[Level(B)];
+	if(CekBatasPasukan(*B) && Owner(*B)!=0){
+		if(Type(*B)=='C') Army(*B)+=AC[Level(*B)];
+		else if(Type(*B)=='T') Army(*B)+=AT[Level(*B)];
+		else if(Type(*B)=='F') Army(*B)+=AF[Level(*B)];
+		else if(Type(*B)=='V') Army(*B)+=AV[Level(*B)];
 	}
 }
 // menambahkan A jumlah pasukan ke seluruh bangunan yang dimiliki X pada setiap turn X
 
 void ChangeOwnerB(Bangunan *B, int X, int Y){
-    Level(B) = 1;
-    Army(B) = X;
-    Owner(B) = Y;
+    Level(*B) = 1;
+    Army(*B) = X;
+    Owner(*B) = Y;
 }
 // reset bangunan saat pindah kepemilikan, 
 // dengan X sebagai jumlah pasukan yang baru
 // dan Y sebagai pemilik yang baru
 
-void MovePasukan(BangArr Br, int X, int Y, int pas){
-    Army(Br.arr[X])-=pas;
-    Army(Br.arr[Y])+=pas;
+void MovePasukan(Bangunan *B1, Bangunan *B2, int X){
+	Army(*B1)-=pas;
+    Army(*B2)+=pas;
 }
-// memindahkan pas buah pasukan dari bangunan X ke bangunan Y
+// memindahkan X buah pasukan dari bangunan B1 ke bangunan B2
