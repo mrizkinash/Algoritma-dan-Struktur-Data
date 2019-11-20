1a1f6d6b5630d9c2f0a35b43efb7bb412c0ea970
cacachandrika <chandrika1302@gmail.com>
2019-11-20 12:31:43 +0700

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,12 +1,12 @@
 #ifndef graph_H
 #define graph_H
 
-#include "boolean.h"
+#include "ADT/boolean.h"
 #include "bangunan.h"
 
 #define Nil NULL
 
-typedef Bangunan infotype;
+typedef int infotype;
 typedef struct tElmtlist *address;
 typedef struct tElmtlist2 *address2;
 
@@ -98,72 +98,16 @@ void InsVLastG (MultiList *G, infotype X);
 /* menambahkan elemen list di akhir: elemen terakhir yang baru */
 /* bernilai X jika alokasi berhasil. Jika alokasi gagal: I.S.= F.S. */
 
-//void InsVFirstCon (MultiList *G, address P, infotype Y);
-
-void InsVLastCon (MultiList *G, address P, infotype X);
-
-
-/*** PENGHAPUSAN ELEMEN ***/
-//void DelVFirstG (MultiList *G, infotype *X);
-/* I.S. List L tidak kosong  */
-/* F.S. Elemen pertama list dihapus: nilai info disimpan pada X */
-/*      dan alamat elemen pertama di-dealokasi */
-//void DelVLastG (MultiList *G, infotype *X);
-/* I.S. list tidak kosong */
-/* F.S. Elemen terakhir list dihapus: nilai info disimpan pada X */
-/*      dan alamat elemen terakhir di-dealokasi */
-//void DelVFirstCon (MultiList *G, address P,infotype *Y);
-
-//void DelVLastCon (MultiList *G, address P, infotype *Y);
-
 
 /****************** PRIMITIF BERDASARKAN ALAMAT ******************/
 /*** PENAMBAHAN ELEMEN BERDASARKAN ALAMAT ***/
-//void InsertFirstG (MultiList *G, address P);
-/* I.S. Sembarang, P sudah dialokasi  */
-/* F.S. Menambahkan elemen ber-address P sebagai elemen pertama */
-//void InsertAfterG (MultiList *G, address P, address Prec);
-/* I.S. Prec pastilah elemen list dan bukan elemen terakhir, */
-/*      P sudah dialokasi  */
-/* F.S. Insert P sebagai elemen sesudah elemen beralamat Prec */
 void InsertLastG (MultiList *G, address P);
 /* I.S. Sembarang, P sudah dialokasi  */
 /* F.S. P ditambahkan sebagai elemen terakhir yang baru */
 void InsertFirstCon (MultiList *G, address P1, address2 P2);
 /* I.S. Sembarang, P sudah dialokasi  */
 /* F.S. Menambahkan elemen ber-address P sebagai elemen pertama */
-//void InsertAfterCon (MultiList *G, address P1, address2 P2, address2 Prec);
-/* I.S. Prec pastilah elemen list dan bukan elemen terakhir, */
-/*      P sudah dialokasi  */
-/* F.S. Insert P sebagai elemen sesudah elemen beralamat Prec */
-//void InsertLastCon (MultiList *G, address P1, address2 P);
-/* I.S. Sembarang, P sudah dialokasi  */
-/* F.S. P ditambahkan sebagai elemen terakhir yang baru */
-
-/*** PENGHAPUSAN SEBUAH ELEMEN ***/
-//void DelFirstG (MultiList *G, address *P);
-/* I.S. List tidak kosong */
-/* F.S. P adalah alamat elemen pertama list sebelum penghapusan */
-/*      Elemen list berkurang satu (mungkin menjadi kosong) */
-/* First element yg baru adalah suksesor elemen pertama yang lama */
-//void DelPG (MultiList *G, infotype X);
-/* I.S. Sembarang */
-/* F.S. Jika ada elemen list beraddress P, dengan Info(P)=X  */
-/* Maka P dihapus dari list dan di-dealokasi */
-/* Jika ada lebih dari satu elemen list dengan Info bernilai X */
-/* maka yang dihapus hanya elemen pertama dengan Info = X */
-/* Jika tidak ada elemen list dengan Info(P)=X, maka list tetap */
-/* List mungkin menjadi kosong karena penghapusan */
-//void DelLastG (MultiList *G, address *P);
-/* I.S. List tidak kosong */
-/* F.S. P adalah alamat elemen terakhir list sebelum penghapusan  */
-/*      Elemen list berkurang satu (mungkin menjadi kosong) */
-/* Last element baru adalah predesesor elemen terakhir yg lama, */
-/* jika ada */
-//void DelAfterG (MultiList *G, address *Pdel, address Prec);
-/* I.S. List tidak kosong. Prec adalah anggota list  */
-/* F.S. Menghapus Next(Prec): */
-/*      Pdel adalah alamat elemen list yang dihapus  */
+void InsVLastCon (MultiList *G, address P, infotype X);
 
 /****************** PROSES SEMUA ELEMEN LIST ******************/
 int NbElmtCon (MultiList G, infotype X);
