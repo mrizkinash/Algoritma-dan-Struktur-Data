d1279f7092d469bfc5b2a4de40960dff798afe15
cacachandrika <chandrika1302@gmail.com>
2019-11-22 09:15:23 +0700

    Update player.h

--- player.h
+++ player.h
@@ -5,9 +5,9 @@ typedef struct {
     Queue skill;
     boolean turn;
     List listbangunan;
-} Bangunan;
+} player;
 
-#define Bangunan(P) P.listbangunan
+#define LB(P) P.listbangunan
 #define Turn(P) P.turn
 #define Skill(P) P.skill
 
